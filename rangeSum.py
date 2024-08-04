5ee91bc937f9af7ba1470c0fa12fac7db4e550f1
Adharsh <contact.adharsh26@gmail.com>
2024-08-04 18:20:15 +0530

    rangeSum.py

---
+++ rangeSum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        
+        MOD = 10**9 + 7
+        n = len(nums)
+        subarray_sums = []
+    
+        # Generate all subarray sums
+        for start in range(n):
+           current_sum = 0
+           for end in range(start, n):
+             current_sum += nums[end]
+             subarray_sums.append(current_sum)
+    
+        # Sort the subarray sums
+        subarray_sums.sort()
+    
+        # Compute the sum of the range
+        result_sum = 0
+        for i in range(left - 1, right):
+          result_sum = (result_sum + subarray_sums[i]) % MOD
+    
+        return result_sum
